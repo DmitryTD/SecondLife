53895134a7f991e762a668eb91b7091da2f6cb5d
ALEHAAAAANDRO <leha.rod.132@gmail.com>
2023-10-21 15:35:38 +0300

    Update index.php

---
+++ src/index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Ваша страница</title>
+    <style>
+        body {
+            background-color: white; 
+            margin: 0;
+            padding: 0;
+        }
+
+        header {
+            background-color: #ffffff; /* Белый хедер */
+            box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.2); /* Тень снизу */
+            padding: 20px;
+        }
+
+        .button {
+            background-color: #00a5cf;
+            color: #ffffff;
+            padding: 10px 20px;
+            text-decoration: none;
+            border-radius: 5px;
+            margin: 10px;
+        }
+    </style>
+</head>
+<body>
+    <header>
+        <!-- Здесь вы можете разместить свои кнопки в хедере -->
+        <a href="#" class="button">Кнопка 1</a>
+        <a href="#" class="button">Кнопка 2</a>
+    </header>
+
+    <!-- Остальной контент вашего сайта идет здесь -->
+    <div>
+        <p>Контент!</p>
+    </div>
+</body>
+</html>
