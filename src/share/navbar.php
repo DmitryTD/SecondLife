1f90a21cdea5499cc0dc875a6184ca59f3912481
ALEHAAAAANDRO <leha.rod.132@gmail.com>
2023-10-21 16:36:36 +0300

    header
    Готовый хэдер для последующих страниц

---
+++ src/share/navbar.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Ваша страница</title>
+    <link rel="stylesheet" href="../css/main.css">
+    
+</head>
+<body>
+    <header class="header">
+        <img src="./share/logo-header.svg" alt="Логотип" class="logo">
+        <div class="buttons">
+            <a href="#" class="button">Кнопка 1</a>
+            <a href="#" class="button">Кнопка 2</a>
+            <a href="#" class="button">Кнопка 3</a>
+            <a href="#" class="button">Кнопка 4</a>
+        </div>
+		<div class="bbuttons">
+        <a href="#" class="button blue-button">Синяя кнопка 1</a>
+        <a href="#" class="button blue-button">Синяя кнопка 2</a>
+		</div>
+    </header>
+</body>
+</html>
