62fb10985515653939c963eb3e55ab2468d96182
ALEHAAAAANDRO <leha.rod.132@gmail.com>
2023-10-22 11:41:43 +0300

    Events
    Добавлена лента событий

--- src/events.php
+++ src/events.php
@@ -1,14 +1,61 @@
 <!DOCTYPE html>
 <html>
-<link rel="stylesheet" href="./css/main.css">
-<link rel="stylesheet" href="./css/events.css">
-<?php
-include './share/navbar.php';
-?>
+<head>
+    <link rel="stylesheet" href="./css/main.css">
+    <link rel="stylesheet" href="./css/events.css">
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            background-color: #f0f0f0;
+            margin: 0;
+            padding: 0;
+        }
 
+        h2 {
+            text-align: center;
+            color: #333;
+            margin: 20px;
+        }
 
+        .image {
+            display: block;
+            margin: 0 auto;
+            max-width: 100%;
+            height: auto;
+            border: 3px solid #fff;
+            box-shadow: 0 3px 6px rgba(0, 0, 0, 0.1);
+        }
 
+        figure {
+            background-color: #fff;
+            padding: 20px;
+            border-radius: 10px;
+            box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2);
+            margin: 20px auto;
+			width: 70%;
+			align: center;
+        }
 
+        figcaption {
+            text-align: center;
+            color: #666;
+            margin: 10px 0;
+        }
+    </style>
+</head>
+<body>
+    <?php include './share/navbar.php'; ?>
+    
+    <h2>Благотворительный забег "Самое время жить!"</h2>
+    <figure>
+        <img class="image" src="./share/events1.jpg" alt="Изображение 1" style="width: 600px; height: auto">
+        <figcaption>Участники благотворительного забега "Самое время жить!" пробегут маршрут для освещения проблемы нехватки Доноров костного мозга</figcaption>
+    </figure>
 
-
-
+    <h2>Поздравляем с Днем Донора!</h2>
+    <figure>
+        <img class="image" src="./share/events2.jpg" alt="Изображение 2" style="width: 600px; height: auto">
+        <figcaption>Празднование Дня Донора</figcaption>
+    </figure>
+</body>
+</html>
