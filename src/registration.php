64af7f355cdc9ab7d6b5de7d1005e39f4261268a
DmitryTD <79697994+DmitryTD@users.noreply.github.com>
2023-10-22 03:43:26 +0300

    fix reg

--- src/registration.php
+++ src/registration.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html>
+
 <head>
     <link rel="stylesheet" href="./css/main.css">
     <?php
@@ -12,6 +13,7 @@
             background-color: #f0f0f0;
             text-align: center;
         }
+
         .container {
             max-width: 400px;
             margin: 0 auto;
@@ -21,94 +23,98 @@
             box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
             margin-top: 20px;
         }
+
         .rounded-input {
             border: 1px solid #ccc;
             border-radius: 5px;
             padding: 10px;
             width: 95%;
         }
+
         h1 {
             font-size: 24px;
             margin-bottom: 20px;
         }
+
         form {
             text-align: left;
         }
+
         .margin {
             margin: 10px 5px 0;
         }
     </style>
 </head>
+
 <body>
-<div class="container">
-    <?php
-    session_start();
+    <div class="container">
+        <?php
+        session_start();
 
-    // Подключение к базе данных
-    try {
-        $db = new PDO('pgsql:host=188.225.24.138;dbname=default_db', 'gen_user', 'nRoDXEp~*p!)N3');
-        $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        echo "";
-    } catch (PDOException $e) {
-        echo "Ошибка соединения с базой данных: " . $e->getMessage();
-    }
+        // Подключение к базе данных
+        try {
+            $db = new PDO('pgsql:host=188.225.24.138;dbname=default_db', 'gen_user', 'nRoDXEp~*p!)N3');
+            $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            echo "";
+        } catch (PDOException $e) {
+            echo "Ошибка соединения с базой данных: " . $e->getMessage();
+        }
 
-    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-        if (isset($_POST['username']) && isset($_POST['password']) && isset($_POST['login'])) {
-            // Вход
-            $email = $_POST['username'];
-            $password = $_POST['password'];
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            if (isset($_POST['username']) && isset($_POST['password']) && isset($_POST['login'])) {
+                // Вход
+                $email = $_POST['username'];
+                $password = $_POST['password'];
 
-            // Поле email, которое вы ищете
+                // Поле email, которое вы ищете
 
-try {
-    // Подготовленный запрос для поиска строки
-$stmt = $db->prepare("SELECT * FROM Users WHERE email = :email");
-    $stmt->bindParam(':email', $email, PDO::PARAM_STR);
-    $stmt->execute();
+                try {
+                    // Подготовленный запрос для поиска строки
+                    $stmt = $db->prepare("SELECT * FROM Users WHERE email = :email");
+                    $stmt->bindParam(':email', $email, PDO::PARAM_STR);
+                    $stmt->execute();
 
-    // Запись результата в переменную
-    $row = $stmt->fetch(PDO::FETCH_ASSOC);
+                    // Запись результата в переменную
+                    $row = $stmt->fetch(PDO::FETCH_ASSOC);
 
-            if ($password === $row['password']) {
-                // Вход выполнен успешно
-                $_SESSION['user'] = $row['email']; // Сохраняем email пользователя в сессии
-                echo 'Вход выполнен успешно!';
-                exit;
-            } else {
-                echo 'Неправильное имя пользователя или пароль';
-            }
-}
-catch (Exception $e) {
-    echo "Непредвиденная ошибка";
-} finally {
-    // Код, который будет выполнен в любом случае, независимо от того, произошло исключение или нет
-}
-        } elseif (isset($_POST['new_email']) && isset($_POST['new_password']) && isset($_POST['register'])) {
-            // Регистрация
-            $newemail = $_POST['new_email'];
-            $newPassword = $_POST['new_password'];
-            $phone = $_POST['phone'];
-            $fio = $_POST['fio'];
+                    if (trim($password) === trim($row['password'])) {
+                        // Вход выполнен успешно
+                        $_SESSION['user'] = $row['email']; // Сохраняем email пользователя в сессии
+                        echo 'Вход выполнен успешно!';
+                        exit;
+                    } else {
+                        echo 'Неправильное имя пользователя или пароль';
+                    }
+                } catch (Exception $e) {
+                    echo "Непредвиденная ошибка";
+                } finally {
+                    // Код, который будет выполнен в любом случае, независимо от того, произошло исключение или нет
+                }
+            } elseif (isset($_POST['new_email']) && isset($_POST['new_password']) && isset($_POST['register'])) {
+                // Регистрация
+                $newemail = $_POST['new_email'];
+                $newPassword = $_POST['new_password'];
+                $phone = $_POST['phone'];
+                $fio = $_POST['fio'];
 
-            // Выполнение запроса для вставки нового пользователя в таблицу
-            $stmt = $db->prepare('INSERT INTO Users (email, password, phonenumber, fio, level) VALUES (?, ?, ?, ?, 1)');
-            if ($stmt->execute([$newemail, $newPassword, $phone, $fio])) {
-                echo 'Регистрация успешно завершена. Теперь вы можете войти.';
-            } else {
-                echo 'Произошла ошибка при регистрации.';
+                // Выполнение запроса для вставки нового пользователя в таблицу
+                $stmt = $db->prepare('INSERT INTO Users (email, password, phonenumber, fio, level) VALUES (?, ?, ?, ?, 1)');
+                if ($stmt->execute([$newemail, $newPassword, $phone, $fio])) {
+                    echo 'Регистрация успешно завершена. Теперь вы можете войти.';
+                } else {
+                    echo 'Произошла ошибка при регистрации.';
+                }
             }
         }
-    }
 
-    if (isset($_SESSION['user'])) {
-        // User is already authenticated
-        echo '<h1>Привет, ' . $_SESSION['user'] . '!</h1>';
-        echo '<p><a href="logout.php" class="button blue-button">Выйти</a></p>';
-    } else {
-        // User is not authenticated, display login and registration forms
-        echo '<h1>Вход</h1>';
-        echo '<form action="" method="post">
+        if (isset($_SESSION['user'])) {
+            // User is already authenticated
+            echo '<h1>Привет, ' . $_SESSION['user'] . '!</h1>';
+            echo '<p><a href="logout.php" class="button blue-button">Выйти</a></p>';
+        } else {
+            // User is not authenticated, display login and registration forms
+            echo '<h1>Вход</h1>';
+            echo '<form action="" method="post">
             <label for="username">Почта:</label>
             <input type="text" name="username" required class="rounded-input">
             <br>
@@ -118,8 +124,8 @@ catch (Exception $e) {
             <input type="submit" name="login" value="Войти" class="margin button blue-button">
         </form>';
 
-        echo '<h1>Регистрация</h1>';
-        echo '<form action="" method="post">
+            echo '<h1>Регистрация</h1>';
+            echo '<form action="" method="post">
             <label for="new_email">Почта:</label>
             <input type="text" name="new_email" required class="rounded-input">
             <br>
@@ -134,8 +140,9 @@ catch (Exception $e) {
             <br>
             <input type="submit" name="register" value="Зарегистрироваться" class="margin button blue-button">
         </form>';
-    }
-    ?>
-</div>
+        }
+        ?>
+    </div>
 </body>
+
 </html>
